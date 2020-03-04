4403d0cfb43483e5b7f523212a242772680264c6
bexpe <beata.k.puzio@gmail.com>
2020-03-04 12:19:13 +0100

    Add gallery component

---
+++ src/components/gallery/gallery.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '../container';
+import Heading from '../Heading';
+import MemoryGame from '../memory-game';
+import TicTacToeGame from '../tic-tac-toe-game';
+
+const Gallery = ({ target }) => (
+    <Container>
+        <Heading>galeria</Heading>
+        <MemoryGame />
+        <TicTacToeGame />
+    </Container>
+);
+export default Gallery;
