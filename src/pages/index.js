ec4fefef298b3fd1b224132a607c266b63108ed2
bexpe <beata.k.puzio@gmail.com>
2020-03-04 12:50:32 +0100

    Add modified index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,26 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Banner from "../components/banner"
+import AboutMe from "../components/about-me"
+import Quote from "../components/quote"
+import Experience from "../components/experience"
+import Gallery from "../components/gallery/gallery"
+import Skills from "../components/skills"
+import Contact from "../components/contact"
+import Footer from "../components/footer"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Banner />
+    <AboutMe />
+    <Quote />
+    <Experience />
+    <Skills />
+    <Gallery />
+    <Contact />
+    <Footer />
   </Layout>
 )
 
