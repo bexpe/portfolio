ca8f1d1996ff4edbef905511556ddb7c5dd1416f
bexpe <beata.k.puzio@gmail.com>
2020-03-04 12:42:28 +0100

    Add footer component

---
+++ src/components/footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+    background-color: #e1e1e1;
+    background-size: cover;
+    background-position: center;
+    color: #3b3b3b;
+    line-height: 10vw;
+    text-align: center;
+
+    @media (orientation: portrait) {
+        background-color: #d5d6db;
+        line-height: 15vw;
+    }
+`
+
+const StyledP = styled.p`
+    font-size: 1.5vw;
+
+    @media (orientation: portrait) {
+        font-size: 2.5vw;
+    }
+`;
+
+const Footer = () => (
+    <StyledWrapper>
+        <StyledP>© Copyright 2020. All Rights Reserved.</StyledP>
+    </StyledWrapper>
+
+);
+
+export default Footer;
